90fc49fa48c3b1af3bb87775a66438e4a41ce59d
ArtemYakubovich <artempaulavich@gmail.com>
2022-06-13 18:17:00 +0300

    change Trajectory

--- Assets/Scripts/Gate.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Gate : MonoBehaviour
-{
-    [SerializeField] private GameObject goal;
-}
--- Assets/Scripts/Plane.cs
+++ Assets/Scripts/Plane.cs
@@ -6,6 +6,7 @@ public class Plane : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
+
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * _speed);
